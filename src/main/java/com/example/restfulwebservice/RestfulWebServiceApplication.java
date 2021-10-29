0a2ae6efe6a5aa1e27e76c82bae42300d92c39aa
백민수 <iphonex@kakao.com>
2021-10-29 21:19:52 +0900

    다국어처리 적용

--- src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
+++ src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
@@ -2,12 +2,25 @@ package com.example.restfulwebservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulWebServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestfulWebServiceApplication.class, args);
+    }
+
+    // Bean을 등록하면 스프링부트가 초기화될때 해당 설정이 메모리에 올라간다.
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.KOREA);
+        return localeResolver;
     }
 
 }
--- src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,11 +1,19 @@
 package com.example.restfulwebservice.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     // GET
     // /hello-world (endPoint)
@@ -26,4 +34,9 @@ public class HelloWorldController {
         return new HelloWorldBean(String.format("hello World, %s", name));
     }
 
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required=false) Locale locale) {
+        // RequestHeader = header의 Accept-Language를 파라미터로 받아서 locale로 지정 , required : ture - 필수/ false - 필수아님
+        return messageSource.getMessage("greeting.message", null, locale);
+    }
 }

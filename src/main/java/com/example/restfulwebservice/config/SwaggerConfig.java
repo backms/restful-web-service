0a93206efedce0bf97dd37f9773a39538e4d388c
DESKTOP-6GSK6G8\future <iphonex@kakao.com>
2021-11-01 16:03:16 +0900

    swagger 소스 추가

--- src/main/java/com/example/restfulwebservice/config/SwaggerConfig.java
+++ src/main/java/com/example/restfulwebservice/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.example.restfulwebservice.config;
 
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
@@ -12,20 +13,27 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configurable
 @EnableSwagger2
+@EnableWebMvc
 public class SwaggerConfig implements WebMvcConfigurer {
 
     @Bean
     public Docket api() {
 
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
 
     }
-    public void addResourceHandler(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources");
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
-
     }
-
 }

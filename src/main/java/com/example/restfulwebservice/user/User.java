9cffa9ca56bbda4ff1aec7ff871b725a8eabdfb1
백민수 <iphonex@kakao.com>
2021-11-07 21:37:43 +0900

    jpa 설정 추가

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -1,8 +1,5 @@
 package com.example.restfulwebservice.user;
 
-import com.fasterxml.jackson.annotation.JsonFilter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
@@ -10,6 +7,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -21,8 +21,11 @@ import java.util.Date;
 @NoArgsConstructor      // 디폴트 생성자
 //@JsonFilter("UserInfo")
 @ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min=2, message = "Name은 2글자 이상 입력해주세요.")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values (1, sysdate(), 'USER1', 'test1', '123456-1234566');
+insert into user values (2, sysdate(), 'USER2', 'test2', '123456-1234566');
+insert into user values (3, sysdate(), 'USER3', 'test3', '123456-1234566');
